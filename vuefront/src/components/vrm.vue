c79998cbf1f13dba1ed1a53938479213dbc3ea0c
won-js <wonjs0116@gmail.com>
2020-12-04 15:18:35 +0900

    vrm background

--- vuefront/src/components/vrm.vue
+++ vuefront/src/components/vrm.vue
@@ -20,15 +20,22 @@ export default {
       light: undefined,
       loader: undefined,
       currentVrm: undefined,
+      geometry: undefined,
+      cubeMaterial: undefined,
+      cubeMaterials: undefined,
+      cube: undefined,
       stage: 0,
       clock
     }
   },
   methods: {
     init: function() {
-      //camera
       const canvas = document.getElementById("container");
+      
+      //scene
       this.scene = new THREE.Scene()
+
+      //camera
       this.camera = new THREE.PerspectiveCamera(
         75,
         // window.innerWidth / window.innerHeight,
@@ -55,12 +62,12 @@ export default {
       this.controls.target.set(0.0,1.0,0.0);
       this.controls.update();
 
-      // scene
+      // light
       this.light = new THREE.DirectionalLight(0xffffff);
       this.light.position.set(1.0,1.0,1.0).normalize();
       this.scene.add(this.light);
 
-      // light
+      // loader
       this.loader = new GLTFLoader();
       this.loader.crossOrigin = "anonymous"
       this.loader.load(
@@ -78,9 +85,24 @@ export default {
           }
       )
 
+      // skybox
+      this.geometry = new THREE.CubeGeometry( 1000, 1000, 1000 );
+      this.cubeMaterials = [
+        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "./images/posx.jpg" ), side: THREE.DoubleSide }),
+        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "./images/negx.jpg" ), side: THREE.DoubleSide }), 
+        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "./images/posy.jpg" ), side: THREE.DoubleSide }), 
+        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "./images/negy.jpg" ), side: THREE.DoubleSide }), 
+        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "./images/posz.jpg" ), side: THREE.DoubleSide }), 
+        new THREE.MeshBasicMaterial({ map: new THREE.TextureLoader().load( "./images/negz.jpg" ), side: THREE.DoubleSide }), 
+      ];
+
+      this.cubeMaterial = new THREE.MeshFaceMaterial( this.cubeMaterials );
+      this.cube = new THREE.Mesh( this.geometry, this.cubeMaterial );
+      this.scene.add( this.cube );
+
       // 좌표 그리기
-      const gridHelper = new THREE.GridHelper(10,10);
-      this.scene.add(gridHelper);
+      // const gridHelper = new THREE.GridHelper(10,10);
+      // this.scene.add(gridHelper);
       // const axeHelper = new THREE.AxesHelper(5);
       // this.scene.add(axeHelper);
     
@@ -89,9 +111,12 @@ export default {
       requestAnimationFrame(this.actionHello);
       // tweak bones
       const deltaTime = this.clock.getDelta();
+      const s = 0.25 * Math.PI * Math.sin(Math.PI * this.clock.elapsedTime);
       if(this.currentVrm !== undefined && this.stage==0) {
-        const s = 0.25 * Math.PI * Math.sin(Math.PI * this.clock.elapsedTime);
-        // console.log(this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.LeftUpperArm).rotation.z);
+        //face
+        this.currentVrm.blendShapeProxy.setValue(VRMSchema.BlendShapePresetName.Joy, 0.5 - 0.5*s);
+        
+        //body
         this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.Neck).rotation.x = s/4;
         this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.LeftUpperArm).rotation.z = s/4;
         this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.LeftLowerArm).rotation.z = s+30;
@@ -99,10 +124,10 @@ export default {
         this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.RightUpperArm).rotation.z = 11.2;
         //update vrm
         this.currentVrm.update( deltaTime );
+
       }else if(this.currentVrm !== undefined && this.stage==1){
         // leftAction
 
-        const s = 0.25 * Math.PI * Math.sin(Math.PI * this.clock.elapsedTime);
         this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.Neck).rotation.x = s/4;
         this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.RightUpperArm).rotation.z = 20+s/3;
         this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.RightHand).rotation.x = 1;
@@ -112,21 +137,32 @@ export default {
         this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.UpperChest).rotation.z = s/4;
         //update vrm
         this.currentVrm.update( deltaTime );
+
       }else if(this.currentVrm !== undefined && this.stage==2){
         // rightAction
 
-        const s = 0.25 * Math.PI * Math.sin(Math.PI * this.clock.elapsedTime);
+        //face
+        this.currentVrm.blendShapeProxy.setValue(VRMSchema.BlendShapePresetName.Joy, 0);
+        this.currentVrm.blendShapeProxy.setValue(VRMSchema.BlendShapePresetName.A, 0.5 + 0.5*s);
+        this.currentVrm.blendShapeProxy.setValue(VRMSchema.BlendShapePresetName.BlinkL, 0.5 - 0.5*s);
+
+        //body
         this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.Neck).rotation.y = s/4;
         this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.LeftUpperArm).rotation.z = 30.2+s/3;
         this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.LeftHand).rotation.x = 1;
         this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.RightUpperArm).rotation.z = 30.3+s/8;
         this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.RightHand).rotation.x = s/2;
+
         //update vrm
         this.currentVrm.update( deltaTime );
+
       }else if(this.currentVrm !== undefined){
+        // face
+        this.currentVrm.blendShapeProxy.setValue(VRMSchema.BlendShapePresetName.BlinkL, 0);
+        this.currentVrm.blendShapeProxy.setValue(VRMSchema.BlendShapePresetName.Joy, 0, s);
+        
         // draw circle
 
-        const s = 0.25 * Math.PI * Math.sin(Math.PI * this.clock.elapsedTime);
         this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.Neck).rotation.y = s/4;
         this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.LeftUpperArm).rotation.z = 30.5;
         this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.LeftLowerArm).rotation.z = 4.8;
@@ -137,9 +173,11 @@ export default {
         this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.RightHand).rotation.x = 3.9;
         this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.RightShoulder).rotation.z = -s/12;
         this.currentVrm.humanoid.getBoneNode(VRMSchema.HumanoidBoneName.Spine).rotation.z = s/4;
+        
         //update vrm
         this.currentVrm.update( deltaTime );        
       }
+      
       this.renderer.render(this.scene, this.camera)
     },
     nextPose() {
